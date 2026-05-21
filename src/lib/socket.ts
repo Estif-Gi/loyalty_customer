ac89a5862d2c3ee580d8975023ace5c010a9c61c
GebetaEst <estifanos@mulewave.com>
2026-05-21 16:29:40 +0300

    coolify 4

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,7 +2,7 @@ import { io, type Socket } from "socket.io-client";
 import { loyaltyStore } from "@/lib/store";
 import type { User } from "@/types";
 
-const SOCKET_URL ="http://localhost:5001";
+const SOCKET_URL =VITE_SOCKET_URL || "https://estif.bahirandelivery.com/";
 let socket: Socket | null = null;
 
 interface ProfileDataPayload {
